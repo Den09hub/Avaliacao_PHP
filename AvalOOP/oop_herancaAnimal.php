2804b0c8543f60203db499805492e21cb22849a0
Denyel09Dev <denyel.1005@gmail.com>
2025-03-13 21:08:12 -0300

    13/03/2025-1

--- AvalOOP/oop_herancaAnimal.php
+++ AvalOOP/oop_herancaAnimal.php
@@ -2,16 +2,24 @@
 
 class Animal{
     public function fazerSom($som){
-        
+        public $nome;
     }
 }
 
 class Cachorro extends Animal{
-
+    public function somDog{
+        echo "AU AU";
+    }
 }
 
 class Gato extends Animal{
-    
+    public function somCat{
+        echo "MIAU";
+    }
 }
 
+$cachorro1 = new Cachorro;
+$cachorro1->somDog();
+
+$gato1 = new Gato; 
 ?>
