530d7f17c0603f6176f217d0382489a44548b6fe
Den09hub <denyel.1005@gmail.com>
2025-03-18 16:10:26 -0400

    18/03/2025

--- AvalOOP/oop_contaBanco.php
+++ AvalOOP/oop_contaBanco.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 class Conta{
     public $titular;
     public $saldo;
@@ -30,5 +30,60 @@ $user1_conta->setDepositar(5000);
 $user1_conta->getConsultar();
 $user1_conta->setSacar(2500);
 $user1_conta->getConsultar();
+*/
+?>
 
+<!-- CORREÇÃO -->
+<?php
+class ContaBancaria {
+    // Atributos da classe
+    private $titular; // Nome do titular da conta
+    private $saldo;   // Saldo da conta
+
+    // Construtor para inicializar os atributos
+    public function __construct($titular, $saldoInicial = 0) {
+        $this->titular = $titular;
+        $this->saldo = $saldoInicial;
+    }
+
+    // Método para depositar dinheiro
+    public function depositar($valor) {
+        if ($valor > 0) {
+            $this->saldo += $valor;
+            echo "Depósito de R$ {$valor} realizado com sucesso. Saldo atual: R$ {$this->saldo}.<br>";
+        } else {
+            echo "Valor de depósito inválido.<br>";
+        }
+    }
+
+    // Método para sacar dinheiro
+    public function sacar($valor) {
+        if ($valor > 0 && $valor <= $this->saldo) {
+            $this->saldo -= $valor;
+            echo "Saque de R$ {$valor} realizado com sucesso. Saldo atual: R$ {$this->saldo}.<br>";
+        } else {
+            echo "Saldo insuficiente ou valor inválido para saque.<br>";
+        }
+    }
+
+    // Método para consultar o saldo
+    public function consultarSaldo() {
+        echo "O saldo da conta de {$this->titular} é R$ {$this->saldo}.<br>";
+    }
+}
+
+// Criando uma instância da classe ContaBancaria
+$conta = new ContaBancaria("João da Silva", 1000);
+
+// Consultar saldo inicial
+$conta->consultarSaldo();
+
+// Realizar um depósito
+$conta->depositar(500);
+
+// Realizar um saque
+$conta->sacar(300);
+
+// Consultar saldo final
+$conta->consultarSaldo();
 ?>
--- AvalOOP/oop_herancaAnimal.php
+++ AvalOOP/oop_herancaAnimal.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 class Animal{
     public function fazerSom(){
         echo "Som genérico do animal";
@@ -23,4 +23,43 @@ $gato1 = new Gato;
 
 $cachorro1->fazerSom();
 $gato1->fazerSom();
+*/
+?>
+
+<!-- CORREÇÃO -->
+
+<?php
+
+// Classe Animal
+class Animal {
+    // Método fazerSom que será sobrescrito pelas classes filhas
+    public function fazerSom() {
+        echo "Som do animal\n";
+    }
+}
+
+// Classe Cachorro que herda de Animal
+class Cachorro extends Animal {
+    // Sobrescreve o método fazerSom
+    public function fazerSom() {
+        echo "Au Au\n";
+    }
+}
+
+// Classe Gato que herda de Animal
+class Gato extends Animal {
+    // Sobrescreve o método fazerSom
+    public function fazerSom() {
+        echo "Miau\n";
+    }
+}
+
+// Criando objetos das classes Cachorro e Gato
+$cachorro = new Cachorro();
+$gato = new Gato();
+
+// Chamando o método fazerSom() para cada objeto
+$cachorro->fazerSom(); // Saída: Au Au
+$gato->fazerSom();     // Saída: Miau
+
 ?>
